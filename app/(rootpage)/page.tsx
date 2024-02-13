40608481469dd9132f80afca18bc1802ad6220c9
gm-mehdi <mehdi.elgoummadi@gmail.com>
2024-02-13 21:05:03 +0100

    feat: fixed header error and moved header and footer to layout file

--- app/(rootpage)/page.tsx
+++ app/(rootpage)/page.tsx
@@ -1,21 +1,17 @@
 import Hero from "@/components/rootpage/hero/Hero";
-import Header from "@/components/rootpage/header/Header";
 import Pillars from "@/components/rootpage/pillars/Pillars";
 import Spaces from "@/components/rootpage/spaces/Spaces";
 import Faq from "@/components/rootpage/faq/Faq";
-import Footer from "@/components/rootpage/footer/Footer";
 import Projects from "@/components/rootpage/projects/Projects";
 
 export default function Home() {
   return (
-    <main>
-      <Header/>
+    <div>
       <Hero/>
       <Pillars/>
       <Spaces/>
       <Projects/>
       <Faq/>
-      <Footer/>
-    </main>
+    </div>
   )
 }
---
+++ app/(rootpage)/projects/page.tsx
@@ -0,0 +1,14 @@
+// import Footer from '@/components/rootpage/footer/Footer'
+// import Header from '@/components/rootpage/header/Header'
+
+const page = () => {
+  return (
+    <div>
+      <p>
+        LISTE PROJECTS
+      </p>
+    </div>
+  )
+}
+
+export default page
