782599eb0dff837f2f2497df70d6ce1696dc608c
ayaoulhint <oulhintaya702@gmail.com>
2024-02-20 14:16:26 +0100

    added new page for list of projects

---
+++ app/lib/interface.ts
@@ -0,0 +1,9 @@
+export interface simpleBlogCard {
+    title: string;
+    slug: String;
+    description: string;
+    currentSlug: string;
+    projectimage: any;
+    content: any;
+    show: boolean;
+}
--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,7 +1,15 @@
 import { createClient } from "next-sanity";
+import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
     projectId: 'm9vl4wqn',
     dataset: 'production',
+    apiVersion: '2024-02-20',
     useCdn: process.env.NODE_ENV === 'production',
-})
+});
+
+const builder = imageUrlBuilder(client)
+
+export function urlFor(source: any){
+    return builder.image(source);
+}
