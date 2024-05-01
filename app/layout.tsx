8883096b9bf801c66c87d529c99671ffcc5758dd
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2024-05-01 12:03:47 +0100

    fix: fixed ts types errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,39 @@
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import './globals.css'
-import Header from '@/components/rootpage/header/Header'
-import Footer from '@/components/rootpage/footer/Footer'
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/rootpage/header/Header";
+import Footer from "@/components/rootpage/footer/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const poppins = Poppins({ subsets: ['latin'], weight: ["400", "600", "700", "500", "200", "300"] })
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["400", "600", "700", "500", "200", "300"],
+});
 
 export const metadata: Metadata = {
-  title: 'Fablab Universiapolis',
-  description: 'A hub where ideas materialize. Our fabrication lab merges technology and creativity. Join us in shaping the future through hands-on exploration at FabLab Universiapolis.',
-}
+    title: "Fablab Universiapolis",
+    description:
+        "A hub where ideas materialize. Our fabrication lab merges technology and creativity. Join us in shaping the future through hands-on exploration at FabLab Universiapolis.",
+    icons: {
+        icon: "/logo.svg",
+    },
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-      <html lang="en">
-          <ClerkProvider>
-              <body className={`${poppins.className} min-h-screen w-full`}>
-                  <Header />
-                  <main>{children}</main>
-                  <Footer />
-              </body>
-          </ClerkProvider>
-      </html>
-  );
+    return (
+        <html lang="en">
+            <ClerkProvider>
+                <body className={`${poppins.className} min-h-screen w-full`}>
+                    <link rel="icon" href="/logo.svg" />
+                    <Header />
+                    <main>{children}</main>
+                    <Footer />
+                </body>
+            </ClerkProvider>
+        </html>
+    );
 }
--- lib/actions/resend.actions.ts
+++ lib/actions/resend.actions.ts
@@ -2,40 +2,52 @@
 import { format } from "date-fns";
 import { Resend } from "resend";
 
-export async function sendConfirmation({ email, date, period }: any) {
+interface ConfirmationParams {
+    email: string;
+    date: Date;
+    period: keyof typeof periodsMap;
+}
+
+const periodsMap = {
+    "1": "09:00 - 12:00",
+    "2": "12:00 - 15:00",
+    "3": "15:00 - 18:00",
+    "4": "18:00 - 21:00",
+} as const;
+
+export async function sendConfirmation({
+    email,
+    date,
+    period,
+}: ConfirmationParams) {
     const resend = new Resend(process.env.RESEND_API_KEY);
-	const periodsMap = {
-		"1": "09:00 - 12:00",
-		"2": "12:00 - 15:00",
-		"3": "15:00 - 18:00",
-		"4": "18:00 - 21:00",
-	};
+    const periodTime = periodsMap[period] || periodsMap["1"];
     const content = `
-	<!DOCTYPE html>
-	<html>
-		<head>
-			<title>Confirmation de Réservation</title>
-			<style>
-				body { font-family: Arial, sans-serif; line-height: 1.6; color: #333; }
-				.container { width: 80%; margin: auto; padding: 20px; background-color: #f7f7f7; border: 1px solid #dcdcdc; border-radius: 8px; }
-				h1 { color: #4CAF50; }
-				p { margin-bottom: 10px; }
-			</style>
-		</head>
-		<body>
-			<div class="container">
-				<h1>Confirmation de Réservation</h1>
-				<p>Bonjour,</p>
-				<p>Merci pour votre réservation. Voici les détails de votre réservation :</p>
-				<p><strong>Date :</strong>${format(date, "PPP")}</p>
-				<p><strong>Période :</strong>${periodsMap[period]}</p>
-				<p>Si vous avez des questions, n'hésitez pas à nous contacter à tout moment.</p>
-				<p>Merci,<br>Fablab Universiapolis</p>
-				<p><strong>PS :</strong> N'oubliez pas votre réservation car toute absence sera déduite de vos notes. Pour annuler, veuillez contacter <a href="mailto:ayoub.moufid@e-polytechnique.ma">ayoub.moufid@e-polytechnique.ma</a>.</p>
-			</div>
-		</body>
-	</html>
-	`;
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Confirmation de Réservation</title>
+            <style>
+                body { font-family: Arial, sans-serif; line-height: 1.6; color: #333; }
+                .container { width: 80%; margin: auto; padding: 20px; background-color: #f7f7f7; border: 1px solid #dcdcdc; border-radius: 8px; }
+                h1 { color: #4CAF50; }
+                p { margin-bottom: 10px; }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <h1>Confirmation de Réservation</h1>
+                <p>Bonjour,</p>
+                <p>Merci pour votre réservation. Voici les détails de votre réservation :</p>
+                <p><strong>Date :</strong>${format(date, "PPP")}</p>
+                <p><strong>Période :</strong>${periodTime}</p>
+                <p>Si vous avez des questions, n'hésitez pas à nous contacter à tout moment.</p>
+                <p>Merci,<br>Fablab Universiapolis</p>
+                <p><strong>PS :</strong> N'oubliez pas votre réservation car toute absence sera déduite de vos notes. Pour annuler, veuillez contacter <a href="mailto:ayoub.moufid@e-polytechnique.ma">ayoub.moufid@e-polytechnique.ma</a>.</p>
+            </div>
+        </body>
+    </html>
+    `;
     const { data } = await resend.emails.send({
         from: "Fablab <onboarding@resend.dev>",
         to: [email],
